2076655236dd9b15439635fc2b27ca2840bea5ca
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-08-20 22:49:39 +0200

    ...

---
+++ src/com/luxlunaris/cincia/frontend/ast/expressions/postfix/ReassignmentExpression.java
@@ -0,0 +1,18 @@
+package com.luxlunaris.cincia.frontend.ast.expressions.postfix;
+
+import com.luxlunaris.cincia.frontend.ast.interfaces.Expression;
+import com.luxlunaris.cincia.frontend.ast.interfaces.PostfixExpression;
+import com.luxlunaris.cincia.frontend.ast.tokens.operator.Operators;
+
+/**
+ * 
+ * Stuff like x++, x--, x+=1, x/=2 ...
+ *
+ */
+public class ReassignmentExpression implements PostfixExpression{
+	
+	public Operators operator;
+	public Expression right;
+	public PostfixExpression arg;
+	
+}
