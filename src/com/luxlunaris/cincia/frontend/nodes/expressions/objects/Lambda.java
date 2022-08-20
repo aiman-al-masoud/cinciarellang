a9831e6880cb96cae05b6f6fbc52228541ef9027
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-08-20 11:37:16 +0200

    ...

--- src/com/luxlunaris/cincia/frontend/nodes/expressions/objects/Lambda.java
+++ src/com/luxlunaris/cincia/frontend/nodes/expressions/objects/Lambda.java
@@ -1,13 +1,8 @@
 package com.luxlunaris.cincia.frontend.nodes.expressions.objects;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.luxlunaris.cincia.frontend.nodes.declarations.MultiDeclaration;
 import com.luxlunaris.cincia.frontend.nodes.declarations.Signature;
 import com.luxlunaris.cincia.frontend.nodes.expressions.Expression;
 import com.luxlunaris.cincia.frontend.nodes.statements.CompoundStatement;
-import com.luxlunaris.cincia.frontend.tokens.Modifier;
 
 //example: // public \ x:int:int -> 2*x
 public class Lambda implements Expression{
