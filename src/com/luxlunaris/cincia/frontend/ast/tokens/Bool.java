8681458c701511b616cf5ac59e6713f42d5de1b6
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-08-20 19:00:38 +0200

    ...

--- src/com/luxlunaris/cincia/frontend/ast/tokens/Bool.java
+++ src/com/luxlunaris/cincia/frontend/ast/tokens/Bool.java
@@ -1,8 +1,12 @@
 package com.luxlunaris.cincia.frontend.ast.tokens;
 
+
 public class Bool extends AbstractToken{
 
 	public final boolean value;
+	public static final String TRUE = "true";
+	public static final String  FALSE = "false";
+
 
 	public Bool(boolean value) {
 		this.value  =value;
@@ -10,11 +14,11 @@ public class Bool extends AbstractToken{
 
 	public static Boolean stringToBool(String string) {
 
-		boolean isTrue = string.equals("true")? true : false;
-		boolean isFalse = string.equals("false")? true : false;
+		boolean isTrue = string.equals(TRUE)? true : false;
+		boolean isFalse = string.equals(FALSE)? true : false;
+		boolean isNull = !(isTrue || isFalse); //neither true nor false
+		return isNull ? null : isTrue? true : false;
 //		Boolean b = isTrue? true : (isFalse? false : null); 
-		Boolean b = !(isTrue || isFalse) ? null : isTrue? true : false;
-		return b;
 	}
 
 }
--- src/com/luxlunaris/cincia/frontend/tokenstream/Test.java
+++ src/com/luxlunaris/cincia/frontend/tokenstream/Test.java
@@ -15,6 +15,8 @@ public class Test {
 		source = "final x = 'ciao mondo'";
 		source = "private x: int = 1 ";
 		source = "f = f\"df\";";
+		source = "x = true || false";
+
 
 
 		
