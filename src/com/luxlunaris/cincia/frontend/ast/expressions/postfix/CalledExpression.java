29ab12b6e3bf9828f31a40b6b86d13f09e63e75d
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-08-20 22:51:22 +0200

    ...

--- src/com/luxlunaris/cincia/frontend/ast/expressions/postfix/CalledExpression.java
+++ src/com/luxlunaris/cincia/frontend/ast/expressions/postfix/CalledExpression.java
@@ -1,12 +1,12 @@
 package com.luxlunaris.cincia.frontend.ast.expressions.postfix;
 
-import com.luxlunaris.cincia.frontend.ast.expressions.MultiExpression;
+//import com.luxlunaris.cincia.frontend.ast.expressions.MultiExpression;
 import com.luxlunaris.cincia.frontend.ast.interfaces.Expression;
 import com.luxlunaris.cincia.frontend.ast.interfaces.PostfixExpression;
 
 public class CalledExpression implements PostfixExpression {
 	
 	public PostfixExpression callable;
-	public MultiExpression args;
+	public Expression args;
 	
 }
