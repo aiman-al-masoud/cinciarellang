14e46f578abda207f826468704ab88d6a4680de5
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-08-20 22:45:29 +0200

    ...

--- src/com/luxlunaris/cincia/frontend/ast/expressions/unary/DestructuringExpression.java
+++ src/com/luxlunaris/cincia/frontend/ast/expressions/unary/DestructuringExpression.java
@@ -2,6 +2,11 @@ package com.luxlunaris.cincia.frontend.ast.expressions.unary;
 
 import com.luxlunaris.cincia.frontend.ast.interfaces.Expression;
 
+/**
+ * 
+ * *arg
+ * 
+ */
 public class DestructuringExpression implements Expression{
 
 	public Expression arg;
--- src/com/luxlunaris/cincia/frontend/ast/expressions/unary/MinusExpression.java
+++ src/com/luxlunaris/cincia/frontend/ast/expressions/unary/MinusExpression.java
@@ -2,6 +2,11 @@ package com.luxlunaris.cincia.frontend.ast.expressions.unary;
 
 import com.luxlunaris.cincia.frontend.ast.interfaces.UnaryExpression;
 
+/**
+ * 
+ * -arg
+ *
+ */
 public class MinusExpression implements UnaryExpression{
 
 	public UnaryExpression arg;
