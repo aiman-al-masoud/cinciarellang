59dc9fafab9e9b1e679c240880f36bedb445ca37
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-08-20 23:26:00 +0200

    ...

--- src/com/luxlunaris/cincia/frontend/ast/expressions/objects/ListExpression.java
+++ src/com/luxlunaris/cincia/frontend/ast/expressions/objects/ListExpression.java
@@ -1,9 +1,12 @@
 package com.luxlunaris.cincia.frontend.ast.expressions.objects;
 
+import com.luxlunaris.cincia.frontend.ast.expressions.MultiExpression;
 import com.luxlunaris.cincia.frontend.ast.interfaces.Expression;
 import com.luxlunaris.cincia.frontend.ast.interfaces.ObjectExpression;
 import com.luxlunaris.cincia.frontend.ast.interfaces.PrimaryExpression;
 
 public class ListExpression implements ObjectExpression{
-
+	
+	public MultiExpression elements; // could include DestructuringExpression
+	
 }
