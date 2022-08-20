d2a20c06ff9e41697bfd12e482e3498fb79eec2c
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-08-20 23:37:29 +0200

    ...

--- src/com/luxlunaris/cincia/frontend/ast/expressions/objects/DictExpression.java
+++ src/com/luxlunaris/cincia/frontend/ast/expressions/objects/DictExpression.java
@@ -1,11 +1,33 @@
 package com.luxlunaris.cincia.frontend.ast.expressions.objects;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.luxlunaris.cincia.frontend.ast.expressions.unary.DestructuringExpression;
 import com.luxlunaris.cincia.frontend.ast.interfaces.Expression;
 import com.luxlunaris.cincia.frontend.ast.interfaces.ObjectExpression;
-import com.luxlunaris.cincia.frontend.ast.interfaces.PrimaryExpression;
+import java.util.Map.Entry;
 
 public class DictExpression implements ObjectExpression{
 	
+	//order doesn't matter
+	public List<Entry<Expression, Expression>> entries;
+	public List<DestructuringExpression> destructs;
 	
+	
+	public DictExpression() {
+		entries = new ArrayList<Map.Entry<Expression, Expression>>();
+		destructs = new ArrayList<DestructuringExpression>();
+	}
+	
+	public void addEntry(Expression key, Expression val) {
+		entries.add(Map.entry(key, val));
+	}
+	
+	public void addDestruct(DestructuringExpression exp) {
+		destructs.add(exp);
+	}
+
 	
 }
