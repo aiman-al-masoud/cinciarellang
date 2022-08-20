24414f3286982c441cd9140b970e7e8a59494c2f
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-08-20 22:44:30 +0200

    ...

---
+++ src/com/luxlunaris/cincia/frontend/ast/expressions/unary/DestructuringExpression.java
@@ -0,0 +1,8 @@
+package com.luxlunaris.cincia.frontend.ast.expressions.unary;
+
+import com.luxlunaris.cincia.frontend.ast.interfaces.Expression;
+
+public class DestructuringExpression implements Expression{
+
+	public Expression arg;
+}
