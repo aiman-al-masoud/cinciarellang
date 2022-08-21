252e2a3e0631d8bc5fba4bc280b3ded8d4ddfec2
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-08-21 12:18:16 +0200

    ...

--- src/com/luxlunaris/cincia/frontend/ast/interfaces/Token.java
+++ src/com/luxlunaris/cincia/frontend/ast/interfaces/Token.java
@@ -1,5 +1,6 @@
 package com.luxlunaris.cincia.frontend.ast.interfaces;
 
+//TODO: only identifier (variable) or const can be a Primary Expression (NOT kw, op, punc...)
 public interface Token extends PrimaryExpression{
 
 }
