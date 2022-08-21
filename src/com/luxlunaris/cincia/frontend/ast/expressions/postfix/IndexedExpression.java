ad80888e7274b5159ffc5ed8685fe27cb1beef87
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-08-21 12:06:06 +0200

    ...

--- src/com/luxlunaris/cincia/frontend/ast/expressions/postfix/IndexedExpression.java
+++ src/com/luxlunaris/cincia/frontend/ast/expressions/postfix/IndexedExpression.java
@@ -4,7 +4,7 @@ import com.luxlunaris.cincia.frontend.ast.interfaces.Expression;
 import com.luxlunaris.cincia.frontend.ast.interfaces.LeftValue;
 import com.luxlunaris.cincia.frontend.ast.interfaces.PostfixExpression;
 
-public class IndexedExpression implements PostfixExpression , LeftValue{
+public class IndexedExpression implements PostfixExpression, LeftValue{
 	
 	public PostfixExpression indexable;
 	public Expression indexOrIterable;
