66d4e1ab047c01a3dfd5d48658c8bcafda381b57
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-08-21 11:50:42 +0200

    ...

--- src/com/luxlunaris/cincia/frontend/ast/statements/ImportStatement.java
+++ src/com/luxlunaris/cincia/frontend/ast/statements/ImportStatement.java
@@ -1,15 +1,32 @@
 package com.luxlunaris.cincia.frontend.ast.statements;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import com.luxlunaris.cincia.frontend.ast.expressions.postfix.DotExpression;
 import com.luxlunaris.cincia.frontend.ast.interfaces.Statement;
 import com.luxlunaris.cincia.frontend.ast.tokens.Identifier;
 import com.luxlunaris.cincia.frontend.ast.tokens.Str;
 
+/**
+ * 
+ * import foo.bar as bar, blurf.foo as f from "path/to/file"
+ *
+ */
 public class ImportStatement implements Statement{
 	
-	public Identifier imported;
+	public List<Entry<DotExpression, Identifier>> imports;
 	public Str fromPath;
-	public Identifier alias;
 	
+	public ImportStatement() {
+		imports = new ArrayList<Map.Entry<DotExpression,Identifier>>();
+	}
+	
+	public void addImport(DotExpression imported, Identifier alias) {
+		imports.add(Map.entry(imported, alias)); //alias could be null
+	}
 	
 	
 }
