d2b22be34af53c8c00d82022089281016f850540
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-08-22 11:27:59 +0200

    still working on parser...

--- src/com/luxlunaris/cincia/frontend/ast/expressions/objects/InterfaceExpression.java
+++ src/com/luxlunaris/cincia/frontend/ast/expressions/objects/InterfaceExpression.java
@@ -1,5 +1,6 @@
 package com.luxlunaris.cincia.frontend.ast.expressions.objects;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.luxlunaris.cincia.frontend.ast.declarations.MultiDeclaration;
@@ -7,11 +8,18 @@ import com.luxlunaris.cincia.frontend.ast.declarations.Signature;
 import com.luxlunaris.cincia.frontend.ast.interfaces.Declaration;
 import com.luxlunaris.cincia.frontend.ast.interfaces.Expression;
 import com.luxlunaris.cincia.frontend.ast.interfaces.ObjectExpression;
+import com.luxlunaris.cincia.frontend.ast.tokens.modifier.Modifier;
 
 public class InterfaceExpression implements ObjectExpression{
 	
+	public List<Modifier> modifiersList; // can be empty	
 //	public List<MultiDeclaration> attributeDeclarations;
 //	public List<Signature> methodDeclaration;
 	public List<Declaration> declarations;
+	
+	public InterfaceExpression() {
+		modifiersList = new ArrayList<Modifier>();
+		declarations = new ArrayList<Declaration>();
+	}
 	
 }
