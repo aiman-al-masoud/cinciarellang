6d3c3d9d6cabc3adc117551f6eab159f5f001e74
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-08-25 18:43:39 +0200

    ...

--- src/com/luxlunaris/cincia/frontend/ast/statements/jump/ReturnStatement.java
+++ src/com/luxlunaris/cincia/frontend/ast/statements/jump/ReturnStatement.java
@@ -1,19 +1,10 @@
 package com.luxlunaris.cincia.frontend.ast.statements.jump;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import com.luxlunaris.cincia.frontend.ast.interfaces.Expression;
 import com.luxlunaris.cincia.frontend.ast.interfaces.Statement;
 
 public class ReturnStatement implements Statement{
 	
-//	public List<Expression> values;
 	public Expression expression;
-	
-	public ReturnStatement() {
-//		values = new ArrayList<Expression>();
-	}
 	
 	@Override
 	public Statement simplify() {
@@ -26,7 +17,4 @@ public class ReturnStatement implements Statement{
 		return "return "+expression+";";
 	}
 	
-//	public void addValue(Expression value) {
-//		this.values.add(value);
-//	}
 }
