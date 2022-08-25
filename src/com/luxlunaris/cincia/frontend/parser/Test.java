80b66c80b0a5226824248bb22d34e6d19132e0ef
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-08-25 23:13:15 +0200

    ...

--- src/com/luxlunaris/cincia/frontend/parser/Test.java
+++ src/com/luxlunaris/cincia/frontend/parser/Test.java
@@ -21,22 +21,22 @@ public class Test {
 		
 		
 		Map<String, String> tests = new HashMap<String, String>();
-		tests.put("1;", "1;");
-		tests.put("f  = \\x:int -> 1;", "");
-		tests.put("x = z = {'y' : 222, 'capra' : 1, 'buruf' : 'hallo123' };", "");
-		tests.put("x = [1,2,3];", "");
+		tests.put("1;", "1");
+		tests.put("f  = \\x:int -> 1;", "(f = [] \\([] x:INT) : null->1)");
+		tests.put("x = z = {'y' : 222, 'capra' : 1, 'buruf' : 'hallo123' };", "(x = (z = {'y' : 222, 'capra' : 1, 'buruf' : 'hallo123'}))");
+		tests.put("x = [1,2,3];", "(x = [1, 2, 3])");
 		tests.put("x[0] = z = a = [1,2,3];", "(x[0] = (z = (a = [1, 2, 3])))");
-		tests.put("x = { 1 : 2 };", "");
-		tests.put("x = { 1-1*1 : 2 };", "");
-		tests.put("f = \\x -> 1;", "");
+		tests.put("x = { 1 : 2 };", "(x = {1 : 2})");
+		tests.put("x = { 1-1*1 : 2 };", "(x = {(1 MINUS (1 ASTERISK 1)) : 2})");
+		tests.put("f = \\x -> 1;", "(f = [] \\([] x:null) : null->1)");
 		tests.put("a = b = c = 1;", "(a = (b = (c = 1)))");
-		tests.put("x = z = {1:2 , * x, 'capra': 3212, };", "");
-		tests.put("for i in x{ x = do(x); x+=1;  }", "");
-		tests.put("dec get final static private foo:int, x:float;", "");
-		tests.put("dec get final static private foo:int;", "");
-		tests.put("dec f:\\x:int:int;", "");
-		tests.put("x == 1 ? 3 : 4;", "");
-		tests.put("match x{case 1: return 1; case 2: return 2;}", "");
+		tests.put("x = z = {1:2 , * x, 'capra': 3212, };", "(x = (z = {1 : 2, 'capra' : 3212, *(x)}))");
+		tests.put("for i in x{ x = do(x); x+=1;  }", "for [i] in x{(x = do(x)); (x PLUS_ASSIGN 1)}");
+		tests.put("dec get final static private foo:int, x:float;", "[([GET, FINAL, STATIC, PRIVATE] foo:INT), ([] x:FLOAT)]");
+		tests.put("dec get final static private foo:int;", "([GET, FINAL, STATIC, PRIVATE] foo:INT)");
+		tests.put("dec f:\\x:int:int;", "([] f \\([] x:INT) : INT)");
+		tests.put("x == 1 ? 3 : 4;", "((x COMPARE 1)?3:4)");
+		tests.put("match x{case 1: return 1; case 2: return 2;}", "match [case 1 {return 1;}, case 2 {return 2;}] null");
 		
 		
 		for(Entry<String, String> e : tests.entrySet()) {
