5b469d133789b5d7003508ceb6cd40db2d57e22b
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-08-26 02:36:38 +0200

    Check if is dec without hideous regexes!

--- src/com/luxlunaris/cincia/frontend/preprocessor/transformations/AddDecKeyword.java
+++ src/com/luxlunaris/cincia/frontend/preprocessor/transformations/AddDecKeyword.java
@@ -9,23 +9,7 @@ import com.luxlunaris.cincia.frontend.tokenstream.TokenStream;
 
 public class AddDecKeyword {
 	
-	
-	
 	public static String applyLine(final String statement) {
-		
-//		String DEC = "dec";
-//		String output = statement;		
-//		String identifier = "([a-zA-Z_][a-zA-Z0-9_]*)";
-//		String modifiers = "("+identifier+"\\s*)*";
-//		String type = ":\\s*"+"(\\\\)*\\s*"+identifier+"\\s*(:"+identifier+")*";
-//		String singleDec = modifiers+"\\s*"+identifier+"\\s*"+type;
-//		String multiDec = "("+singleDec+"\\s*,"+singleDec+")*";
-//		
-//		if(  statement.matches(singleDec+";") || statement.matches(multiDec+";") ) {
-//			output = statement.replaceFirst("\\s*"+DEC+"\\s*", "");
-//			output = "dec "+output;
-//		}
-
 		
 		String DEC = "dec";
 		String ifDec = statement.replaceFirst("\\s*"+DEC+"\\s*", "");
