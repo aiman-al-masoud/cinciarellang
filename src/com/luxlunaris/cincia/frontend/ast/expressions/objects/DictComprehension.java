eb09480d0aae2bee814ffb5bc40c2894b91e7b3b
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-08-26 23:32:23 +0200

    ...

--- src/com/luxlunaris/cincia/frontend/ast/expressions/objects/DictComprehension.java
+++ src/com/luxlunaris/cincia/frontend/ast/expressions/objects/DictComprehension.java
@@ -5,9 +5,11 @@ import com.luxlunaris.cincia.frontend.ast.interfaces.Expression;
 import com.luxlunaris.cincia.frontend.ast.interfaces.ObjectExpression;
 
 public class DictComprehension implements ObjectExpression{
-
+	
+	// {key : val `for` source `in` iterable `where` where}
 	public Expression key;
 	public Expression val;
+	public Expression source;
 	public Expression iterable;
 	public Expression where; //optional
 	
@@ -16,8 +18,14 @@ public class DictComprehension implements ObjectExpression{
 	public Expression simplify() {
 		this.key = key.simplify();
 		this.val = val.simplify();
+		this.source = source.simplify();
 		this.iterable = iterable.simplify();
-		this.where = where.simplify();
+		
+		
+		if(where!=null) {
+			this.where = where.simplify();
+		}
+		
 		return this;
 	}
 	
