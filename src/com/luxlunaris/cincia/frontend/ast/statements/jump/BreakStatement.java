e91622d058ee0f4b6a4aa1b2d3316df15dfaac49
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-08-27 11:25:37 +0200

    ...

--- src/com/luxlunaris/cincia/frontend/ast/statements/jump/BreakStatement.java
+++ src/com/luxlunaris/cincia/frontend/ast/statements/jump/BreakStatement.java
@@ -1,12 +1,18 @@
 package com.luxlunaris.cincia.frontend.ast.statements.jump;
 
 import com.luxlunaris.cincia.frontend.ast.interfaces.Statement;
+import com.luxlunaris.cincia.frontend.ast.tokens.keyword.Keywords;
 
 public class BreakStatement implements Statement{
 	
 	@Override
 	public Statement simplify() {
 		return this;
+	}
+	
+	@Override
+	public String toString() {
+		return Keywords.BREAK.toString();
 	}
 	
 }
--- src/com/luxlunaris/cincia/frontend/ast/statements/jump/ContinueStatement.java
+++ src/com/luxlunaris/cincia/frontend/ast/statements/jump/ContinueStatement.java
@@ -1,11 +1,17 @@
 package com.luxlunaris.cincia.frontend.ast.statements.jump;
 
 import com.luxlunaris.cincia.frontend.ast.interfaces.Statement;
+import com.luxlunaris.cincia.frontend.ast.tokens.keyword.Keywords;
 
 public class ContinueStatement implements Statement{
 
 	@Override
 	public Statement simplify() {
 		return this;
+	}
+	
+	@Override
+	public String toString() {
+		return Keywords.CONTINUE.toString();
 	}
 }
