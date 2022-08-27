deabe15da01bfdc223a69f81c49ad7fb2a383291
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-08-27 22:48:29 +0200

    ...

--- src/com/luxlunaris/cincia/frontend/parser/Test.java
+++ src/com/luxlunaris/cincia/frontend/parser/Test.java
@@ -65,7 +65,7 @@ public class Test {
 		mE.right = new Int(2);
 		mE.op = Operators.ASTERISK;
 		ExpressionStatement ex = new ExpressionStatement(mE);
-		add("1 * 2;", mE.toString());
+		add("1 * 2;", ex.toString());
 
 		
 		add("1 / 2;", "");
