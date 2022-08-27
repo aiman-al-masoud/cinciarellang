bf1abb99bb47ba84ba34dad386ddc12b5defc436
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-08-27 12:22:09 +0200

    ...

--- src/com/luxlunaris/cincia/frontend/parser/Test.java
+++ src/com/luxlunaris/cincia/frontend/parser/Test.java
@@ -97,6 +97,7 @@ public class Test {
 		// import statement 
 		add("import capra.c as c from \"./files/capre\";", "");
 		add("import capra as c from \"./files/capre\";", "");
+		add("import capra as c, muflone.lana as m from \"./files/capre\";", "");
 
 		
 		for(Entry<String, String> e : tests) {
