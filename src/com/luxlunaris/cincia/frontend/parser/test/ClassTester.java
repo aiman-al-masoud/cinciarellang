e0b24747966c16a5c358995d5dd91e2a5bb5a673
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-08-28 14:35:42 +0200

    ...

--- src/com/luxlunaris/cincia/frontend/parser/test/ClassTester.java
+++ src/com/luxlunaris/cincia/frontend/parser/test/ClassTester.java
@@ -5,9 +5,12 @@ import com.luxlunaris.cincia.frontend.ast.declarations.Signature;
 import com.luxlunaris.cincia.frontend.ast.declarations.VariableDeclaration;
 import com.luxlunaris.cincia.frontend.ast.expressions.binary.AssignmentExpression;
 import com.luxlunaris.cincia.frontend.ast.expressions.objects.ClassExpression;
+import com.luxlunaris.cincia.frontend.ast.expressions.objects.LambdaExpression;
 import com.luxlunaris.cincia.frontend.ast.expressions.type.PrimitiveType;
 import com.luxlunaris.cincia.frontend.ast.statements.DeclarationStatement;
+import com.luxlunaris.cincia.frontend.ast.statements.ExpressionStatement;
 import com.luxlunaris.cincia.frontend.ast.tokens.Identifier;
+import com.luxlunaris.cincia.frontend.ast.tokens.constant.Int;
 import com.luxlunaris.cincia.frontend.ast.tokens.keyword.Keywords;
 
 public class ClassTester extends AbstractTester {
@@ -33,8 +36,15 @@ public class ClassTester extends AbstractTester {
 		
 		AssignmentExpression ase = new AssignmentExpression();
 		ase.left = new Identifier("f");
-		
-		
+		LambdaExpression lE = new LambdaExpression();
+		lE.expression = new Int(1);
+		Signature sg2 = new Signature();
+		VariableDeclaration vD2 = new VariableDeclaration();
+		vD2.name = new Identifier("x");
+		sg2.params = vD2;
+		lE.signature = sg2;
+		ase.right = lE;
+		cE.addStatement(new ExpressionStatement(ase));
  		
 		add("class { x:int; f:\\x:int:int; f = \\x->1; x = 1;  };", cE.toString());
 
