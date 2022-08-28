1526eb5c8818550c5b79d623d2d7ed34aefdfe7a
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-08-28 20:21:27 +0200

    ...

--- src/com/luxlunaris/cincia/frontend/parser/test/RangeExpTester.java
+++ src/com/luxlunaris/cincia/frontend/parser/test/RangeExpTester.java
@@ -7,12 +7,18 @@ public class RangeExpTester extends AbstractTester {
 	
 	public RangeExpTester() {
 		
+		// simple range from 1 to 10
 		RangeExpression rE  = new RangeExpression();
 		rE.from = new Int(1);
 		rE.to = new Int(10);
-		
 		add("1 to 10;", rE.toString());
 		
+		
+		// used as a fancy index in an array
+		
+		
+		
+		add("arr[1 to 4]", );
 		
 		
 		
