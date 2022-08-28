e312b44ba774b03bec5d29398764951dfbd3c0d4
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-08-28 14:43:27 +0200

    ...

--- src/com/luxlunaris/cincia/frontend/parser/test/InterfaceTester.java
+++ src/com/luxlunaris/cincia/frontend/parser/test/InterfaceTester.java
@@ -1,12 +1,22 @@
 package com.luxlunaris.cincia.frontend.parser.test;
 
+import com.luxlunaris.cincia.frontend.ast.expressions.objects.ClassExpression;
+
 public class InterfaceTester extends AbstractTester {
 
 	public InterfaceTester() {
+		
+		ClassExpression cE = new ClassExpression();
+
 
 		// interface 
 		add("interface { x:int; y:int; f:\\x:int:int;  };", "");
 
+		
+		
+		
+		
+		
 	}
 
 }
