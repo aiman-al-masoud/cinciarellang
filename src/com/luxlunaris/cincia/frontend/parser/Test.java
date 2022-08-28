bc78e551ebe6efe3387e3a6ac91bc442436bd45e
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-08-28 13:31:45 +0200

    ...

--- src/com/luxlunaris/cincia/frontend/parser/Test.java
+++ src/com/luxlunaris/cincia/frontend/parser/Test.java
@@ -13,6 +13,7 @@ import com.luxlunaris.cincia.frontend.ast.expressions.binary.AddExpression;
 import com.luxlunaris.cincia.frontend.ast.expressions.binary.AssignmentExpression;
 import com.luxlunaris.cincia.frontend.ast.expressions.binary.ComparisonExpression;
 import com.luxlunaris.cincia.frontend.ast.expressions.binary.MulExpression;
+import com.luxlunaris.cincia.frontend.ast.expressions.objects.DictComprehension;
 import com.luxlunaris.cincia.frontend.ast.expressions.objects.DictExpression;
 import com.luxlunaris.cincia.frontend.ast.expressions.objects.LambdaExpression;
 import com.luxlunaris.cincia.frontend.ast.expressions.objects.ListComprehension;
@@ -180,7 +181,25 @@ public class Test {
 		add("[x+1 for x in l];", lC.toString());
 		
 		
-		add("x = { key : val for v in dict };", "");
+		// dict comprehension
+		DictComprehension dC = new DictComprehension();
+		Identifier en = new Identifier("e");
+		IndexedExpression ine = new IndexedExpression();
+		ine.indexable = en;
+		ine.index = new Int(0);
+		IndexedExpression ine2 = new IndexedExpression();
+		ine2.indexable = en;
+		ine2.index = new Int(1);
+		dC.key = ine;
+		dC.val = ine2;
+		dC.source = en;
+		dC.iterable = new Identifier("entries");
+		AssignmentExpression ase = new AssignmentExpression();
+		ase.left = new Identifier("x");
+		ase.right = dC;
+		add("x = { e[0] : e[1] for e in entries };", ase.toString());
+		
+		
 		add("\\x->1;", "");
 		add("\\x->{ x = 1;y=x+1;return y; };", "");
 		add("class { dec x:int; f = \\x->1;  };", "");
