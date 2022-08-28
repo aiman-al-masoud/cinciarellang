66b5a5b455a55a808d359ea390e9b52bb62cb272
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-08-28 14:21:52 +0200

    ...

--- src/com/luxlunaris/cincia/frontend/parser/test/Test.java
+++ src/com/luxlunaris/cincia/frontend/parser/test/Test.java
@@ -53,30 +53,30 @@ public class Test {
 		//TODO: don't rely on string reprs for tests!!! (right now results are correct but some 'fails' crop up due to literal string comparison)
 
 
-//		add("1;", "1");
-//		add("f  = \\x:int -> 1;", "(f = [] \\([] x:INT) : null->1)");
-//		add("x = z = {'y' : 222, 'capra' : 1, 'buruf' : 'hallo123' };", "(x = (z = {'y' : 222, 'capra' : 1, 'buruf' : 'hallo123'}))");
-//		add("x = [1,2,3];", "(x = [1, 2, 3])");
-//		add("x[0] = z = a = [1,2,3];", "(x[0] = (z = (a = [1, 2, 3])))");
-//		add("x = { 1-1*1 : 2 };", "(x = {(1 MINUS (1 ASTERISK 1)) : 2})");
-//		add("f = \\x -> 1;", "(f = [] \\([] x:null) : null->1)");
-//		add("a = b = c = 1;", "(a = (b = (c = 1)))");
-//		add("x = z = {1:2 , * x, 'capra': 3212, };", "(x = (z = {1 : 2, 'capra' : 3212, *(x)}))");
-//		add("for i in x{ x = do(x); x+=1;  }", "for [i] in x{(x = do(x)); (x PLUS_ASSIGN 1)}");
-//		add("get final static private foo:int, x:float;", "[([GET, FINAL, STATIC, PRIVATE] foo:INT), ([] x:FLOAT)]");
-//		add("get final static private foo:int;", "([GET, FINAL, STATIC, PRIVATE] foo:INT)");
-//		add("f:\\x:int:int;", "([] f \\([] x:INT) : INT)");
-//		add("x:int|float;", "([] x:INT | FLOAT)");
-//		add("dec x:int|float;", "([] x:INT | FLOAT)");
-//		add("x == 1 ? 3 : 4;", "((x COMPARE 1)?3:4)");
-//		add("while true{ x+=1;y+=2; }\n x+=1;", "while true then {(x PLUS_ASSIGN 1); (y PLUS_ASSIGN 2)}");
-//		add("true && false;", "");
-//		add("true || false;", "");
+		//		add("1;", "1");
+		//		add("f  = \\x:int -> 1;", "(f = [] \\([] x:INT) : null->1)");
+		//		add("x = z = {'y' : 222, 'capra' : 1, 'buruf' : 'hallo123' };", "(x = (z = {'y' : 222, 'capra' : 1, 'buruf' : 'hallo123'}))");
+		//		add("x = [1,2,3];", "(x = [1, 2, 3])");
+		//		add("x[0] = z = a = [1,2,3];", "(x[0] = (z = (a = [1, 2, 3])))");
+		//		add("x = { 1-1*1 : 2 };", "(x = {(1 MINUS (1 ASTERISK 1)) : 2})");
+		//		add("f = \\x -> 1;", "(f = [] \\([] x:null) : null->1)");
+		//		add("a = b = c = 1;", "(a = (b = (c = 1)))");
+		//		add("x = z = {1:2 , * x, 'capra': 3212, };", "(x = (z = {1 : 2, 'capra' : 3212, *(x)}))");
+		//		add("for i in x{ x = do(x); x+=1;  }", "for [i] in x{(x = do(x)); (x PLUS_ASSIGN 1)}");
+		//		add("get final static private foo:int, x:float;", "[([GET, FINAL, STATIC, PRIVATE] foo:INT), ([] x:FLOAT)]");
+		//		add("get final static private foo:int;", "([GET, FINAL, STATIC, PRIVATE] foo:INT)");
+		//		add("f:\\x:int:int;", "([] f \\([] x:INT) : INT)");
+		//		add("x:int|float;", "([] x:INT | FLOAT)");
+		//		add("dec x:int|float;", "([] x:INT | FLOAT)");
+		//		add("x == 1 ? 3 : 4;", "((x COMPARE 1)?3:4)");
+		//		add("while true{ x+=1;y+=2; }\n x+=1;", "while true then {(x PLUS_ASSIGN 1); (y PLUS_ASSIGN 2)}");
+		//		add("true && false;", "");
+		//		add("true || false;", "");
 		// binary
 
 
 
-	
+
 
 
 		for(Entry<String, String> e : tests) {
@@ -85,7 +85,7 @@ public class Test {
 			CharStream cS = new CharStream(pP.process());
 			TokenStream tS = new TokenStream(cS);		
 			Parser p  = new Parser(tS);
-			
+
 			try {
 				Statement s = p.parse().get(0).simplify();
 				System.out.println(e.getKey()+" "+ ( e.getValue().equals(s.toString()) ? ok("OK") : fail("FAIL") ) +" "+s);
@@ -126,7 +126,7 @@ public class Test {
 	public static void add(String s1, String s2) {
 		tests.add(Map.entry(s1, s2));
 	}
-	
+
 	public static void add(AbstractTester tester) {
 		tests.addAll(tester.tests);
 	}
