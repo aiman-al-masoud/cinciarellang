c5b996111d0bfaab404b1f538996265c7dc2a1ed
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-08-29 23:24:46 +0200

    ...

---
+++ src/com/luxlunaris/cincia/backend/Lambda.java
@@ -0,0 +1,19 @@
+//package com.luxlunaris.cincia.backend;
+//
+//import java.util.List;
+//
+//import com.luxlunaris.cincia.frontend.ast.statements.CompoundStatement;
+//
+//public class Lambda{
+//
+//	public Lambda(List<String> params, CompoundStatement compStatement) {
+//
+//	}
+//
+//	public Object run(Enviro enviro, ) {
+//
+//		//bind args
+//
+//	}
+//
+//}
