629391b4813f044b05fffa8354aadb7df2954500
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-08-30 23:29:25 +0200

    ...

---
+++ src/com/luxlunaris/cincia/backend/CinciaUndefined.java
@@ -0,0 +1,11 @@
+package com.luxlunaris.cincia.backend;
+
+import com.luxlunaris.cincia.frontend.ast.interfaces.Type;
+
+public class CinciaUndefined extends CinciaObject {
+
+	public CinciaUndefined(Type type) {
+		super(type);
+	}
+
+}
