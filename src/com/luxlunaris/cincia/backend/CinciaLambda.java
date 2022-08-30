7f57889007a075b7a85037857503c9c826ef0b02
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-08-30 23:04:39 +0200

    ...

--- src/com/luxlunaris/cincia/backend/CinciaLambda.java
+++ src/com/luxlunaris/cincia/backend/CinciaLambda.java
@@ -4,18 +4,18 @@ import com.luxlunaris.cincia.frontend.ast.declarations.Signature;
 import com.luxlunaris.cincia.frontend.ast.interfaces.Expression;
 import com.luxlunaris.cincia.frontend.ast.statements.CompoundStatement;
 
-public class Lambda {
+public class CinciaLambda {
 	
 	private CompoundStatement block;
 	private Expression expression;
 	private Signature signature;
 	
 	
-	public Lambda(Signature signature, CompoundStatement block) {
+	public CinciaLambda(Signature signature, CompoundStatement block) {
 		this.block = block;
 	}
 	
-	public Lambda(Signature signature, Expression expression) {
+	public CinciaLambda(Signature signature, Expression expression) {
 		this.expression = expression;
 	}
 	
