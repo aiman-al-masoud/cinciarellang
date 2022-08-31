ec9b2158a7d4fd735be99d5ea66e16c16cee0276
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-08-31 15:19:19 +0200

    ...

--- src/com/luxlunaris/cincia/backend/CinciaClass.java
+++ src/com/luxlunaris/cincia/backend/CinciaClass.java
@@ -2,11 +2,11 @@ package com.luxlunaris.cincia.backend;
 
 import com.luxlunaris.cincia.frontend.ast.interfaces.Type;
 
-public class CinciaClass extends CinciaObject{
+public class CinciaClass /*extends CinciaObject*/{
 
-	public CinciaClass() {
-		super(null);
-	}
+//	public CinciaClass() {
+//		super(null);
+//	}
 	
 	
 //	public CinciaObject constructor() {
