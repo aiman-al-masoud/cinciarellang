d2bb167d7194dc0214bd2919fa3fee2ef687d6e2
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-08-31 12:43:48 +0200

    ...

--- src/com/luxlunaris/cincia/backend/CinciaMethod.java
+++ src/com/luxlunaris/cincia/backend/CinciaMethod.java
@@ -1,6 +1,10 @@
 package com.luxlunaris.cincia.backend;
 
+import java.util.List;
+
+import com.luxlunaris.cincia.backend.CinciaFunction.Eval;
 import com.luxlunaris.cincia.frontend.ast.expressions.type.Signature;
+import com.luxlunaris.cincia.frontend.ast.interfaces.Expression;
 import com.luxlunaris.cincia.frontend.ast.statements.CompoundStatement;
 
 public class CinciaMethod extends CinciaFunction{
@@ -11,12 +15,25 @@ public class CinciaMethod extends CinciaFunction{
 	public CinciaMethod(Signature signature, CompoundStatement block) {
 		super(signature, block);
 	}
+	
+	public CinciaMethod(Signature signature, Expression expression) {
+		super(signature, expression);
+	}
 
 	public void setParent(CinciaObject parent) {
 		this.parent = parent;
 	}
 	
-	
+	/**
+	 * Runs on the ORIGINAL parent object's environment,
+	 * has SIDE EFFECTS!
+	 * @param args
+	 * @param eval
+	 * @return
+	 */
+	public CinciaObject run(List<Expression> args, Eval eval) {
+		return super.run(args, parent.getEnviro(), eval);
+	}
 	
 	
 
