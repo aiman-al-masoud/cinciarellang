80ca06d3fdf7e2fa0b12e8715fea54022c2abb13
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-09-02 00:14:35 +0200

    ...

--- src/com/luxlunaris/cincia/backend/CinciaFloat.java
+++ src/com/luxlunaris/cincia/backend/CinciaFloat.java
@@ -3,6 +3,7 @@ package com.luxlunaris.cincia.backend;
 import com.luxlunaris.cincia.frontend.ast.expressions.type.PrimitiveType;
 import com.luxlunaris.cincia.frontend.ast.interfaces.Type;
 
+//TODO: implement magic methods
 public class CinciaFloat extends AbstractCinciaObject {
 	
 	private double value;
--- src/com/luxlunaris/cincia/backend/CinciaInt.java
+++ src/com/luxlunaris/cincia/backend/CinciaInt.java
@@ -2,6 +2,7 @@ package com.luxlunaris.cincia.backend;
 
 import com.luxlunaris.cincia.frontend.ast.expressions.type.PrimitiveType;
 
+//TODO: implement magic methods
 public class CinciaInt extends AbstractCinciaObject {
 	
 	private int value;
