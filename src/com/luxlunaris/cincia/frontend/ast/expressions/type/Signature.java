73aa16b23965d3e873a3832ce8f7860794fcc839
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-09-01 18:50:55 +0200

    ...

--- src/com/luxlunaris/cincia/frontend/ast/expressions/type/Signature.java
+++ src/com/luxlunaris/cincia/frontend/ast/expressions/type/Signature.java
@@ -1,12 +1,7 @@
 package com.luxlunaris.cincia.frontend.ast.expressions.type;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.luxlunaris.cincia.frontend.ast.interfaces.Declaration;
 import com.luxlunaris.cincia.frontend.ast.interfaces.Type;
-import com.luxlunaris.cincia.frontend.ast.tokens.Identifier;
-import com.luxlunaris.cincia.frontend.ast.tokens.modifier.Modifier;
 
 /**
  * The abstract signature of a lambda function.
@@ -17,7 +12,6 @@ public class Signature implements Type{
 	
 	public Declaration params;// can be null if func takes no args
 	public Type returnType;
-	
 	
 	@Override
 	public String toString() {
