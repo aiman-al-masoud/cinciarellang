a7fdeb076bc7d81d8120b5465f40fb53efc1f24f
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-09-01 18:16:20 +0200

    ...

--- src/com/luxlunaris/cincia/frontend/ast/expressions/type/PrimitiveType.java
+++ src/com/luxlunaris/cincia/frontend/ast/expressions/type/PrimitiveType.java
@@ -1,6 +1,5 @@
 package com.luxlunaris.cincia.frontend.ast.expressions.type;
 
-import com.luxlunaris.cincia.frontend.ast.interfaces.Type;
 import com.luxlunaris.cincia.frontend.ast.tokens.keyword.Keywords;
 
 public class PrimitiveType extends OneNameType{
