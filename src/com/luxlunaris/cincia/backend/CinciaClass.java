b773e23ef6c512cc9a8caae5cb331dfe43bc0205
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-09-01 15:32:10 +0200

    ...

--- src/com/luxlunaris/cincia/backend/CinciaClass.java
+++ src/com/luxlunaris/cincia/backend/CinciaClass.java
@@ -10,36 +10,39 @@ import com.luxlunaris.cincia.frontend.ast.tokens.Identifier;
 
 public class CinciaClass extends CinciaObject{
 
+	
 	public CinciaClass(Type type) {
 		super(type);
 	}
 
-	public void addMethod(String name, CinciaMethod method) {
-		set(name, method, method.type);
-	}	
-	
-	public void addAttribute(String name, CinciaObject value) {
-		set(name, value, value.type);
-	}	
-	
 	public void declareAttribute(String name, Type type) {
 		set(name, null, type);
 	}	
-	
+
 	public void declareMethod(String name, Signature type) {
 		set(name, null, type);
 	}	
-	
 
+
+	public void addMethod(String name, CinciaMethod method) {
+		set(name, method, method.type);
+	}	
+
+	public void addAttribute(String name, CinciaObject value) {
+		set(name, value, value.type);
+	}	
+
+
+	//TODO: remove method duplication (all instances refer to same func objects)
+	//TODO: deal with modifiers such as static
 	public CinciaObject constructor(List<CinciaObject> args) {
-		Enviro newEnv = this.getEnviro().newChild(); //TODO: remove method duplication
-		CinciaObject obj = new CinciaObject(this.type);
+		Enviro newEnv = this.getEnviro().newChild(); 
+		CinciaObject obj = new CinciaObject(this.type);  
 		obj.enviro = newEnv;
 		obj.__init__(args);
 		return obj;
+		
 	}
-
-
 
 
 }
