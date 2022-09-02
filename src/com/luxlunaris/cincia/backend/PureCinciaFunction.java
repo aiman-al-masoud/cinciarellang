a09f9dea8d7bd6c4ee8926e148f6bdc80a68cd58
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-09-02 15:45:01 +0200

    ...

--- src/com/luxlunaris/cincia/backend/PureCinciaFunction.java
+++ src/com/luxlunaris/cincia/backend/PureCinciaFunction.java
@@ -1,6 +1,7 @@
 package com.luxlunaris.cincia.backend;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import com.luxlunaris.cincia.backend.Interpreter.Eval;
 import com.luxlunaris.cincia.frontend.ast.expressions.objects.LambdaExpression;
@@ -10,7 +11,6 @@ import com.luxlunaris.cincia.frontend.ast.expressions.objects.LambdaExpression;
  * Doesn't even read from the global env, its only input are its arguments.
  * And (like regular functions), it can't write to the gloal env.
  * 
- * TODO: make sure it can't be passed args by reference.
  */
 public class PureCinciaFunction extends CinciaFunction {
 
@@ -20,7 +20,9 @@ public class PureCinciaFunction extends CinciaFunction {
 	
 	public CinciaObject run(List<CinciaObject> args) {
 		Enviro enviro = new Enviro(null);
-		return super.run(args, enviro);
+		// TODO: make sure it can't be passed args by reference.
+		List<CinciaObject> argsCopy =  args.stream().map(o->o.copy(null)).collect(Collectors.toList());
+		return super.run(argsCopy, enviro);
 	}
 
 }
