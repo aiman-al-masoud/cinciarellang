37f66c1fa908a6275d41993a1fcc86ae0022b603
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-09-03 11:25:26 +0200

    ...

--- src/com/luxlunaris/cincia/backend/Interpreter.java
+++ src/com/luxlunaris/cincia/backend/Interpreter.java
@@ -400,7 +400,7 @@ public class Interpreter extends AbstractTraversal<CinciaObject> {
 	@Override
 	public CinciaObject evalLambdaExpression(LambdaExpression lambdex, Enviro enviro) {
 
-		// TODO: check if env is class, in that return a method.
+		// Check if env is class, in that return a method.
 		CinciaBool b = (CinciaBool)enviro.get(CinciaClass.IS_CLASS);
 		if(b!=null && b.__bool__()) {
 			return new CinciaMethod(lambdex, this::eval);
