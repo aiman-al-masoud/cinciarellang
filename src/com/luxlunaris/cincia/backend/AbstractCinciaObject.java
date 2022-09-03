3d4a0efdeb33f85e6efec63227ded64b6b4423f7
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-09-03 18:54:57 +0200

    ...

--- src/com/luxlunaris/cincia/backend/AbstractCinciaObject.java
+++ src/com/luxlunaris/cincia/backend/AbstractCinciaObject.java
@@ -218,7 +218,7 @@ public class AbstractCinciaObject implements CinciaObject{
 				
 		AbstractCinciaObject obj = new AbstractCinciaObject(this.type);
 		obj.enviro = new Enviro(this.enviro); 
-		
+		//TODO: wroooong
 
 		return obj;
 	}
