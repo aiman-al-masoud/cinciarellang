af60a95e7898b8af33d1df66d6402226a8a225da
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-09-03 11:59:48 +0200

    ...

--- src/com/luxlunaris/cincia/backend/CinciaList.java
+++ src/com/luxlunaris/cincia/backend/CinciaList.java
@@ -1,15 +1,34 @@
 package com.luxlunaris.cincia.backend;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import com.luxlunaris.cincia.frontend.ast.expressions.type.ListType;
 import com.luxlunaris.cincia.frontend.ast.interfaces.Type;
 
-public class CinciaList extends AbstractCinciaObject {
+public class CinciaList extends AbstractCinciaObject implements Iterable<CinciaObject> {
+	
+	ArrayList<CinciaObject> list;
 
 	public CinciaList(Type type) {
 		super(new ListType(type));
+		list = new ArrayList<CinciaObject>();
+	}
+
+	@Override
+	public Iterator<CinciaObject> iterator() {
+		return list.iterator();
 	}
 	
+	@Override
+	public CinciaObject get(String key) {
+		return list.get(Integer.parseInt(key)); //TODO: fix this stupid solution
+	}
 	
-	
+	@Override
+	public void set(String key, CinciaObject val, Type type) {
+		list.set(Integer.parseInt(key), val);
+	}
+		
 
 }
