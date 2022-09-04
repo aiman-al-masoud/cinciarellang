89676ffc3c60aa36c13e019e20318196742efb64
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-09-04 17:31:28 +0200

    ...

--- src/com/luxlunaris/cincia/backend/CinciaList.java
+++ src/com/luxlunaris/cincia/backend/CinciaList.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
-import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import com.luxlunaris.cincia.frontend.ast.expressions.type.ListType;
