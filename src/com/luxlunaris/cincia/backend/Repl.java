ef3f2dc36e6596c7ec05d9e0dd4bf80ad32f126f
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-09-04 23:52:44 +0200

    ...

--- src/com/luxlunaris/cincia/backend/Repl.java
+++ src/com/luxlunaris/cincia/backend/Repl.java
@@ -12,42 +12,64 @@ import com.luxlunaris.cincia.frontend.tokenstream.TokenStream;
 
 public class Repl {
 
-	public static void main(String[] args) {
+	Enviro enviro;
+	Interpreter interpreter;
+	Scanner scanner;
 
-		Enviro enviro = new Enviro(null);	
-		Interpreter interpreter = new Interpreter();
-		Scanner scanner;
+	public Repl() {
+		enviro = new Enviro(null);	
+		interpreter = new Interpreter();
+		scanner = new Scanner(System.in);
 		enviro.set("print", new CinciaFunction(Repl::printWrapper));
-		
-	
-		
+	}
+
+	public void mainLoop() {
+
 		while(true) {
 			System.out.print(">");
-			scanner = new Scanner(System.in);
 			String source = scanner.nextLine()+";";
-			Preprocessor preprocessor = new Preprocessor(source);
-			CharStream charStream = new CharStream(preprocessor.process());
-			TokenStream tokenStream = new TokenStream(charStream);
-			Parser parser = new Parser(tokenStream);
-			List<Statement> statements = parser.parse();
-			statements = statements.stream().map(s->s.simplify()).collect(Collectors.toList());
-			statements.forEach(s -> {
-				CinciaObject out = interpreter.eval(s, enviro);	
-				
-				if(out!=null) {
-					System.out.println(out);
-				}
-				
-			});
+			eval(source, enviro);
 		}
-		
+
 	}
-	
+
+
+	public static void main(String[] args) {
+
+		Repl r = new Repl();
+		r.mainLoop();
+
+	}
+
 	protected static CinciaObject printWrapper(List<CinciaObject> args) {
 		args.forEach(e->{System.out.print(e);});
 		System.out.println();
 		return null;
 	}
-	
+
+	protected void eval(String source, Enviro enviro) {
+
+		Preprocessor preprocessor = new Preprocessor(source);
+		CharStream charStream = new CharStream(preprocessor.process());
+		TokenStream tokenStream = new TokenStream(charStream);
+		Parser parser = new Parser(tokenStream);
+		List<Statement> statements = parser.parse();
+		statements = statements.stream().map(s->s.simplify()).collect(Collectors.toList());
+		statements.forEach(s -> {
+			CinciaObject out = interpreter.eval(s, enviro);	
+
+			if(out!=null) {
+				System.out.println(out);
+			}
+
+		});
+
+	}
+
+//	protected CinciaObject evalWrapper(List<CinciaObject> args) {
+//		eval(source, enviro, interpreter);
+//	}
+
+
 
 }
