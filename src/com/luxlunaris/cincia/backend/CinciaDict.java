913b2c1364d0a4cff7093e8558765575822ceca1
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-09-04 23:24:22 +0200

    ...

--- src/com/luxlunaris/cincia/backend/CinciaDict.java
+++ src/com/luxlunaris/cincia/backend/CinciaDict.java
@@ -1,5 +1,7 @@
 package com.luxlunaris.cincia.backend;
 
+import java.util.Optional;
+
 import com.luxlunaris.cincia.frontend.ast.expressions.type.DictType;
 import com.luxlunaris.cincia.frontend.ast.interfaces.Type;
 
@@ -7,6 +9,17 @@ public class CinciaDict extends AbstractCinciaObject {
 
 	public CinciaDict(Type keyType, Type valType) {
 		super(new DictType(keyType, valType));
+	}
+	
+	
+	@Override
+	public String toString() {
+		
+		Optional<String> d =  enviro.items().stream()
+											.filter(e->e.getValue()!=this)
+				 	                        .map(e->e.getKey()+" : "+e.getValue())
+				                        	.reduce((e1,e2)->e1+", "+e2);
+		return "{ "+d.get()+" }";
 	}
 
 }
