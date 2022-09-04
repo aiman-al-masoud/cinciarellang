df569b6c2db9590ab21ed290b7fd2ebf86d5643e
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-09-04 17:37:54 +0200

    ...

--- src/com/luxlunaris/cincia/backend/PureCinciaFunction.java
+++ src/com/luxlunaris/cincia/backend/PureCinciaFunction.java
@@ -21,7 +21,7 @@ public class PureCinciaFunction extends CinciaFunction {
 
 	public CinciaObject run(List<CinciaObject> args) {
 		Enviro enviro = new Enviro(null);
-		// Make sure it can't be passed args by reference:
+		// Make sure args can't ever be passed args by reference:
 		List<CinciaObject> argsCopy = args.stream().map(o->o.copy(null)).collect(Collectors.toList());
 		return super.run(argsCopy, enviro);
 	}
