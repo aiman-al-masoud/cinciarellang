2eccdef5e405b509cccedb51463d8ac07bd94ad7
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-09-05 19:31:29 +0200

    ...

--- src/com/luxlunaris/cincia/backend/CinciaFunction.java
+++ src/com/luxlunaris/cincia/backend/CinciaFunction.java
@@ -12,6 +12,7 @@ import com.luxlunaris.cincia.frontend.ast.expressions.type.IdentifierType;
 import com.luxlunaris.cincia.frontend.ast.expressions.type.Signature;
 import com.luxlunaris.cincia.frontend.ast.interfaces.Declaration;
 import com.luxlunaris.cincia.frontend.ast.interfaces.Type;
+import com.luxlunaris.cincia.frontend.ast.tokens.modifier.Modifiers;
 
 public class CinciaFunction extends AbstractCinciaObject implements Callable{
 
@@ -21,7 +22,8 @@ public class CinciaFunction extends AbstractCinciaObject implements Callable{
 	}
 
 	protected LambdaExpression lambdex;
-	protected List<Entry<String, Type>> params;
+	protected List<Entry<String, Type>> paramTypes;
+	protected List<Entry<String, List<Modifiers>>> paramMods;
 	protected Eval eval;
 	protected WrappedFunction wrappedFunction;
 
@@ -29,7 +31,8 @@ public class CinciaFunction extends AbstractCinciaObject implements Callable{
 		super(lambdex.signature);
 		this.eval = eval;
 		this.lambdex = lambdex;
-		params = new ArrayList<Map.Entry<String, Type>>();
+		paramTypes = new ArrayList<Map.Entry<String, Type>>();
+		paramMods = new ArrayList<Map.Entry<String,List<Modifiers>>>();
 		parseParams();
 	}
 
@@ -45,37 +48,42 @@ public class CinciaFunction extends AbstractCinciaObject implements Callable{
 
 			// bind args to env, TODO: check matching types
 			for(int i=0; i < args.size(); i++) {
-				enviro.set(params.get(i).getKey(), args.get(i), params.get(i).getValue());
+				enviro.set(paramTypes.get(i).getKey(), args.get(i), paramTypes.get(i).getValue());
 			}
 
 		}
-
-		if(lambdex.block != null) {
-			return eval.eval(lambdex.block, enviro);
-		}else if(lambdex.expression != null){
-			return eval.eval(lambdex.expression, enviro);
-		}else if(wrappedFunction != null){
+		
+		
+		if(lambdex == null && wrappedFunction != null) {
 			return wrappedFunction.run(args);
+		}else if(lambdex != null && lambdex.block != null) {
+			return eval.eval(lambdex.block, enviro);
+		}else if(lambdex != null && lambdex.expression != null) {
+			return eval.eval(lambdex.expression, enviro);
 		}
 
 		throw new RuntimeException("Lambda without expression nor block!");
 	}
 
 	public void parseParams() {
-
-		Declaration decParams = ((Signature)type).params;
+		
+		if(lambdex==null) {
+			return;
+		}
+		
 		List<SingleDeclaration> declarations = new ArrayList<SingleDeclaration>();
 
 		try {
-			SingleDeclaration sD =(SingleDeclaration)decParams;
+			SingleDeclaration sD =(SingleDeclaration)lambdex.signature.params;
 			declarations.add(sD);
 		}catch (ClassCastException e) {
-			MultiDeclaration mD =(MultiDeclaration)decParams;
+			MultiDeclaration mD =(MultiDeclaration)lambdex.signature.params;
 			declarations.addAll(mD.declarations);
 		}
 		
 		declarations.forEach(d->{
-			params.add(Map.entry(d.getName(), d.getType()));
+			paramTypes.add(Map.entry(d.getName(), d.getType()));
+			paramMods.add(Map.entry(d.getName(), d.getModifiers()));
 		});
 
 	}
