6d2b8ce04d7ef521cd7fed515e11f65c51caff25
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-09-06 00:52:56 +0200

    ...

--- src/com/luxlunaris/cincia/backend/CinciaBool.java
+++ src/com/luxlunaris/cincia/backend/CinciaBool.java
@@ -46,14 +46,16 @@ public class CinciaBool extends PrimitiveCinciaObject {
 	@Override
 	public CinciaObject __and__(CinciaObject other) {
 
-		try {
-			CinciaBool o = (CinciaBool)other;
-			return new CinciaBool(value && o.value);
-		}catch (ClassCastException e) {
-
-		}
-
-		throw new RuntimeException("");
+//		try {
+//			CinciaBool o = (CinciaBool)other;
+//			return new CinciaBool(value && o.value);
+//		}catch (ClassCastException e) {
+//
+//		}
+//
+//		throw new RuntimeException("");
+		
+		return new CinciaBool(value && other.__bool__());
 	}
 	
 	@Override
