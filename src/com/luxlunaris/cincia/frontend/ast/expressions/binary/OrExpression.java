9896f10dce32380ca2127e7996cb61a2e4f46908
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-09-06 00:41:49 +0200

    ...

--- src/com/luxlunaris/cincia/frontend/ast/expressions/binary/OrExpression.java
+++ src/com/luxlunaris/cincia/frontend/ast/expressions/binary/OrExpression.java
@@ -3,34 +3,23 @@ package com.luxlunaris.cincia.frontend.ast.expressions.binary;
 import com.luxlunaris.cincia.frontend.ast.interfaces.Expression;
 
 public class OrExpression extends AssignmentExpression{
-	
+
 	@Override
 	public String toString() {
 		return "("+left+" || "+right+")";
 	}
-	
+
 	@Override
 	public Expression simplify() {
-		
+
 		this.left = left.simplify();
-		
+
 		if(right==null) {
 			return left;
 		}
-		
-//		this.right = (AndExpression) right.simplify();
+
 		this.right = right.simplify();
 		return this;
 	}
-	
-	
-// or other solution
-//	public OrExpression left;
-//	OrExpression e = new OrExpression();
-//	OrExpression e2 = new OrExpression();
-//	e2.left = null;
-//	e2.right = new AndExpression();
-//	e.left = e2;
-//	e.right = new AndExpression();
-	
+
 }
