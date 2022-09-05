3c6a0c931da0f8f9941e056b01e870bf0399273c
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-09-06 00:41:10 +0200

    ...

--- src/com/luxlunaris/cincia/frontend/ast/expressions/binary/MulExpression.java
+++ src/com/luxlunaris/cincia/frontend/ast/expressions/binary/MulExpression.java
@@ -5,28 +5,23 @@ import com.luxlunaris.cincia.frontend.ast.interfaces.UnaryExpression;
 import com.luxlunaris.cincia.frontend.ast.tokens.operator.Operators;
 
 public class MulExpression extends AddExpression{
-	
-//	public Operators op;
-//	public Expression left;
-//	public UnaryExpression right;
-	
-	
+
 	@Override
 	public String toString() {
 		return "("+left+" "+op+" "+right+")";
 	}
-	
+
 	@Override
 	public Expression simplify() {
-		
+
 		this.left = left.simplify();
-		
+
 		if(right==null) {
 			return left;
 		}
-		
+
 		this.right = (UnaryExpression) right.simplify();
 		return this;
 	}
-	
+
 }
