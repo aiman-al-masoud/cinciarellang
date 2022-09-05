2e254766848e488bad3fef7539f739ebe4299477
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-09-06 00:40:56 +0200

    ...

--- src/com/luxlunaris/cincia/frontend/ast/expressions/binary/ComparisonExpression.java
+++ src/com/luxlunaris/cincia/frontend/ast/expressions/binary/ComparisonExpression.java
@@ -4,26 +4,23 @@ import com.luxlunaris.cincia.frontend.ast.interfaces.Expression;
 import com.luxlunaris.cincia.frontend.ast.tokens.operator.Operators;
 
 public class ComparisonExpression extends AndExpression{
-	
-//	public Operators op;
-//	public Expression left;
-//	public Expression right; //Add expression
-	
+
 	@Override
 	public String toString() {
 		return "("+left+" "+op+" "+right+")";
 	}
-	
+
 	@Override
 	public Expression simplify() {
-		this.left = left.simplify();
 		
+		this.left = left.simplify();
+
 		if(right==null) {
 			return left;
 		}
-		
+
 		this.right =  right.simplify();
 		return this;
 	}
-	
+
 }
