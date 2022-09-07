fe1d1a0a9410b248f9af82d4d04c266968c3b357
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-09-07 09:29:45 +0200

    ...

--- src/com/luxlunaris/cincia/ide/Indenter.java
+++ src/com/luxlunaris/cincia/ide/Indenter.java
@@ -1,31 +1,31 @@
 package com.luxlunaris.cincia.ide;
 
 public class Indenter {
-	
+
 	protected String source;
-	
+
 	public Indenter(String source) {
 		this.source = source;
 	}
-	
+
 	public String getIndented() {
-		
+
 		String result = "";
 		String indentation = "";
-		
+
 		for(String line : source.split("\n")) {
 			result+=indentation+line.trim()+"\n";
-			
+
 			if(line.contains("{")) {
 				indentation+="\t";
 			}else {
 				indentation = indentation.length()>0? indentation.charAt(0)+"" : indentation;
 			}
-			
-			
+
+
 		}
-		  
+
 		return result;
 	}
-	
+
 }
