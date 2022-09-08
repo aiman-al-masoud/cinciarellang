07c78bcac9f0a0c374a223c585dbc59480f32dd6
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-09-08 14:50:28 +0200

    ...

--- src/com/luxlunaris/cincia/backend/Test.java
+++ src/com/luxlunaris/cincia/backend/Test.java
@@ -1,31 +1,38 @@
 package com.luxlunaris.cincia.backend;
 
+import java.util.ArrayList;
 import java.util.List;
-
+import java.util.Map;
+import java.util.Map.Entry;
 
 import com.luxlunaris.cincia.frontend.ast.interfaces.Ast;
-import com.luxlunaris.cincia.frontend.ast.interfaces.Statement;
-import com.luxlunaris.cincia.frontend.charstream.CharStream;
-import com.luxlunaris.cincia.frontend.parser.Parser;
-import com.luxlunaris.cincia.frontend.preprocessor.Preprocessor;
-import com.luxlunaris.cincia.frontend.tokenstream.TokenStream;
-
 import com.luxlunaris.cincia.frontend.Compiler;
 
 public class Test {
+	
+	List<Entry<String, CinciaObject>> tests;
+	
+	public Test() {
+		tests = new ArrayList<Entry<String,CinciaObject>>();
+	}
 
 
 	public static void main(String[] args) {
 		
-		System.out.println(assertEquals("1;", new CinciaInt(1)));
-		System.out.println(assertEquals("1==1;", new CinciaBool(true)));
-//		System.out.println(assertEquals("1!=1;", new CinciaBool(false)));
-
-
+		Test t = new Test();
+		t.addTest("1;", new CinciaInt(1));
+		t.addTest("1==1;", new CinciaBool(true));
+		t.addTest("1!=1;", new CinciaBool(false));
+		t.addTest("1| \\x->2*x | \\x->3*x;", new CinciaInt(6));
+		
+		
+		
+		t.runAll();
+		
 	}
 
 
-	public static CinciaObject run(String source) {
+	public CinciaObject run(String source) {
 		
 		Compiler compiler = new Compiler();
 		Enviro enviro = new Enviro(null);
@@ -39,8 +46,29 @@ public class Test {
 		return out;
 	}
 	
-	public static boolean assertEquals(String source, CinciaObject result) {
+	public boolean assertEquals(String source, CinciaObject result) {
 		return run(source).toString().equals(result.toString());				
+	}
+	
+	public void addTest(String source, CinciaObject result) {
+		tests.add(Map.entry(source, result));
+	}
+	
+	public void runAll() {
+		
+		tests.forEach(e->{
+			
+			try {
+				
+				boolean res = assertEquals(e.getKey(), e.getValue());
+				System.out.println(e.getKey()+" "+(res?"PASS!":"FAIL!"));
+				
+			} catch (Exception e2) {
+				System.out.println(e.getKey()+" BROKEN! ");
+//				e2.printStackTrace();
+			}
+			
+		});
 	}
 	
 	
