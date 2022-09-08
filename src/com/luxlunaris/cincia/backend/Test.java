db3a67b2db10de936da3aeb71522fc2050b260c4
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-09-08 14:42:13 +0200

    ...

--- src/com/luxlunaris/cincia/backend/Test.java
+++ src/com/luxlunaris/cincia/backend/Test.java
@@ -2,6 +2,7 @@ package com.luxlunaris.cincia.backend;
 
 import java.util.List;
 
+
 import com.luxlunaris.cincia.frontend.ast.interfaces.Ast;
 import com.luxlunaris.cincia.frontend.ast.interfaces.Statement;
 import com.luxlunaris.cincia.frontend.charstream.CharStream;
@@ -9,43 +10,49 @@ import com.luxlunaris.cincia.frontend.parser.Parser;
 import com.luxlunaris.cincia.frontend.preprocessor.Preprocessor;
 import com.luxlunaris.cincia.frontend.tokenstream.TokenStream;
 
+import com.luxlunaris.cincia.frontend.Compiler;
+
 public class Test {
-	
-	
+
+
 	public static void main(String[] args) {
 		
+		System.out.println(assertEquals("1;", new CinciaInt(1)));
+		System.out.println(assertEquals("1==1;", new CinciaBool(true)));
+//		System.out.println(assertEquals("1!=1;", new CinciaBool(false)));
+
+
+	}
+
+
+	public static CinciaObject run(String source) {
 		
-		String source = "x = 1+1+2+1*3;";
-		source = "x = class { y = 1; };"; //TODO?
-		source = "x = (\\x->1)();";
-		source = "x = (\\x, y->2*x + y)(1, 2) + 1;";
-		
-//		c = class{ __init__ = \x -> 1; crap = 1; }
-//		x = c()
-//		x.crap // 1
-//		x.crap = 2
-//		x.crap // 2
-				
-		Preprocessor preprocessor = new Preprocessor(source);
-		CharStream charStream = new CharStream(preprocessor.process());
-		TokenStream tokenStream = new TokenStream(charStream);
-		Parser parser = new Parser(tokenStream);
-		List<Statement> statements = parser.parse();
+		Compiler compiler = new Compiler();
 		Enviro enviro = new Enviro(null);
 		Interpreter interpreter = new Interpreter();
-		
-		Ast ast = statements.get(0).simplify();
-		
-		interpreter.eval(ast, enviro);		
-		
-		System.out.println(enviro.get("x"));
-		System.out.println(enviro.getType("x"));
+		CinciaObject out = null;
 
-		
+		for(Ast stm : compiler.compile(source)) {
+			out = interpreter.eval(stm, enviro);
+		}
+
+		return out;
+	}
+	
+	public static boolean assertEquals(String source, CinciaObject result) {
+		return run(source).toString().equals(result.toString());				
 	}
 	
 	
 	
 	
-	
+
+
+
+
+
+
+
+
+
 }
