cc00824047764e00be8e72d22bcface4278169e4
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-09-10 16:58:30 +0200

    ...

--- src/com/luxlunaris/cincia/backend/interfaces/CinciaClass.java
+++ src/com/luxlunaris/cincia/backend/interfaces/CinciaClass.java
@@ -6,6 +6,12 @@ import com.luxlunaris.cincia.frontend.ast.interfaces.Type;
 
 public interface CinciaClass extends CinciaObject, Type, Callable{
 	
+	/**
+	 * Returns a new instance of the this class, calls the constructor
+	 * with the args if possible. 
+	 * @param args
+	 * @return
+	 */
 	CinciaObject newInstance(List<CinciaObject> args);
 	
 }
