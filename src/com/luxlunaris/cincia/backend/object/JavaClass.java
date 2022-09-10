6dc8e905e6ab60df83d2962d5610101a14301463
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-09-10 16:32:17 +0200

    ...

--- src/com/luxlunaris/cincia/backend/object/JavaClass.java
+++ src/com/luxlunaris/cincia/backend/object/JavaClass.java
@@ -8,7 +8,7 @@ import java.util.List;
 import com.luxlunaris.cincia.backend.interfaces.CinciaObject;
 
 public class JavaClass extends JavaObject {
-	
+
 	Class clazz;
 
 	public static Class loadClass(String javaClass) {
@@ -30,44 +30,23 @@ public class JavaClass extends JavaObject {
 		super(clazz);
 		this.clazz = clazz;
 	}
-	
+
 
 	public CinciaObject newInstance(List<CinciaObject> args) {
-		
-//		System.out.println(args);
-		
-//		try {
-//			System.out.println("trying to instantiate "+clazz);
-//			@SuppressWarnings("unchecked")
-//			clazz.getClasses()
-//			System.out.println(   Arrays.asList(clazz.getDeclaredConstructors()) );
-			
-			for(Constructor cons : clazz.getDeclaredConstructors()) {
-				
-				try {
-					Object newInst = cons.newInstance(args.stream().map(a-> a.toJava()).toArray());
-					return new JavaObject(newInst);					
-				} catch (Exception e) {
-					
-				}
-				
+
+		for(Constructor cons : clazz.getDeclaredConstructors()) {
+
+			try {
+				Object newInst = cons.newInstance(args.stream().map(a-> a.toJava()).toArray());
+				return new JavaObject(newInst);					
+			} catch (Exception e) {
+
 			}
-			
-			return null;
-			
-//			Arrays.asList(clazz.getDeclaredConstructors())
-			
-//			.forEach(c->{
-//			});
-			
-//			Object newInst = clazz.getDeclaredConstructor().newInstance( args.stream().map(a-> a.toJava()).toArray()  );
-//			return new JavaObject(clazz.newInstance());
-//			return new JavaObject(newInst);
-//		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException e) {
-//			e.printStackTrace();
-//		}
-		
-//		return null;
+
+		}
+
+		return null;
+
 	}
 
 
