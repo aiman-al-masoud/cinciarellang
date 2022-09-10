2265ea939296bf1ed9c9b48149954a10240bb6fc
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-09-10 16:49:04 +0200

    ...

--- src/com/luxlunaris/cincia/backend/object/JavaClass.java
+++ src/com/luxlunaris/cincia/backend/object/JavaClass.java
@@ -12,25 +12,10 @@ import com.luxlunaris.cincia.frontend.ast.interfaces.Type;
 public class JavaClass extends JavaObject implements Type{
 
 	Class clazz;
-
-//	public static Class loadClass(String javaClass) {
-//
-//		try {
-//			return JavaClass.class.getClassLoader().loadClass(javaClass);
-//		} catch (ClassNotFoundException e) {
-//			e.printStackTrace();
-//		}
-//
-//		return null;
-//	}
-
-//	public JavaClass(String javaClass) throws ClassNotFoundException {
-//		super(loadClass(javaClass));
-//	}
+	
 
 	public JavaClass(Class clazz) {
 		super(clazz);
-		
 		this.clazz = clazz;
 	}
 
