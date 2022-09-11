07763d85e92b4fa917048b4f638c646357591a29
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-09-11 23:37:11 +0200

    ...

--- src/com/luxlunaris/cincia/backend/primitives/CinciaString.java
+++ src/com/luxlunaris/cincia/backend/primitives/CinciaString.java
@@ -16,25 +16,40 @@ public class CinciaString extends PrimitiveCinciaObject {
 	public String getValue() {
 		return value;
 	}
-	
+
 	@Override
 	public CinciaObject __add__(CinciaObject other) {
-		
+
 		try {
 			CinciaString otherStr =  (CinciaString)other;
 			return new CinciaString(value+otherStr.value);
 		} catch (ClassCastException e) {
-		
+
 		}
-		
+
 		throw new RuntimeException("Operator + undefined ...");
 	}
-	
+
+
+	@Override
+	public CinciaObject __eq__(CinciaObject other) {
+
+		try {
+			CinciaString otherStr = (CinciaString)other;
+			return new CinciaBool(otherStr.value.equals(value));
+		} catch (Exception e) {
+
+		}
+
+		return new CinciaBool(false);
+	}
+
+
 	@Override
 	public String toString() {
 		return "\""+value+"\"";
 	}
-	
+
 	@Override
 	public String toJava() {
 		return value;
