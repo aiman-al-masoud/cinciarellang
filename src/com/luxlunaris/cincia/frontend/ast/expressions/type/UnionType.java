13e5f04d515c5af720ada8341012a5ce4049e57f
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-09-13 21:04:38 +0200

    ...

--- src/com/luxlunaris/cincia/frontend/ast/expressions/type/UnionType.java
+++ src/com/luxlunaris/cincia/frontend/ast/expressions/type/UnionType.java
@@ -10,28 +10,27 @@ import com.sun.source.tree.Tree;
 
 //TODO: add in EBNF
 public class UnionType implements Type{
-	
+
 	public List<SingleType> types;
-	
+
 	public UnionType() {
-//		System.out.println("creating union type");
 		types = new ArrayList<SingleType>();
 	}
-	
+
 	public void addType(SingleType sT) {
 		types.add(sT);
 	}
-	
+
 	@Override
 	public Expression simplify() {
-				
+
 		if(types.size()==1) {
 			return types.get(0);
 		}
-		
+
 		return this;
 	}
-	
+
 	@Override
 	public String toString() {
 		return types.stream().map(x->x+"").reduce((x1,x2)->x1+" | "+x2).get();
@@ -41,5 +40,5 @@ public class UnionType implements Type{
 	public boolean matches(Type other) {		
 		return types.stream().anyMatch(t->t.matches(other));
 	}
-	
+
 }
