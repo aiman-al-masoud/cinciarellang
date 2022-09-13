5f01be8437ce7919c56b2686132f1f9a58df7a7a
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-09-13 19:06:09 +0200

    ...

--- src/com/luxlunaris/cincia/backend/interfaces/CinciaIterable.java
+++ src/com/luxlunaris/cincia/backend/interfaces/CinciaIterable.java
@@ -3,7 +3,7 @@ package com.luxlunaris.cincia.backend.interfaces;
 import com.luxlunaris.cincia.backend.callables.PureCinciaFunction;
 
 public interface CinciaIterable extends Iterable<CinciaObject>, CinciaObject{
-	
+  	
 	CinciaIterable filter(PureCinciaFunction f);
 	CinciaIterable map(PureCinciaFunction f);
 	CinciaIterable reduce(PureCinciaFunction f, CinciaObject initial);
