0efd85164ae76083f2e1c7b541c45739f72d5c0c
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-09-15 17:50:25 +0200

    ...

--- src/com/luxlunaris/cincia/backend/stdlib/Stdlib.java
+++ src/com/luxlunaris/cincia/backend/stdlib/Stdlib.java
@@ -7,7 +7,7 @@ import com.luxlunaris.cincia.frontend.ast.expressions.type.IdentifierType;
 public class Stdlib extends AbstractCinciaObject{
 
 	public Stdlib() {
-		super(new IdentifierType("Module"));
+		super(new IdentifierType("Module")); //TODO: extract type
 		set("concurrency", new Concurrency());
 	}
 
