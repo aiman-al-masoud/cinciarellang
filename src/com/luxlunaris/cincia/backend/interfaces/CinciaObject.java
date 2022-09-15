0936888693e72197ca64ca65e43399882a409d75
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-09-15 17:24:52 +0200

    ...

--- src/com/luxlunaris/cincia/backend/interfaces/CinciaObject.java
+++ src/com/luxlunaris/cincia/backend/interfaces/CinciaObject.java
@@ -68,7 +68,7 @@ public interface CinciaObject extends Stateful{
 	CinciaObject __neg__() ;
 	CinciaObject __init__(List<CinciaObject> args);
 	CinciaObject as(List<CinciaObject> args); //cast/conversion to other class	
-	CinciaObject copy(List<CinciaObject> args); 
+	CinciaObject copy(List<CinciaObject> args); // returns a deep copy of the object
 	CinciaObject freeze(List<CinciaObject> args);// return an immutable copy of this object
 	Object toJava(); //returns a Java-object equivalent
 	CinciaBool is(List<CinciaObject> args);
