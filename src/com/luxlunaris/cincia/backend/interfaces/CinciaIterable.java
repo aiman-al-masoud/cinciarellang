1cb0a3b58661c8fbea24d08e09777975752fd074
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-09-16 12:21:37 +0200

    ...

--- src/com/luxlunaris/cincia/backend/interfaces/CinciaIterable.java
+++ src/com/luxlunaris/cincia/backend/interfaces/CinciaIterable.java
@@ -7,12 +7,12 @@ import com.luxlunaris.cincia.backend.callables.PureCinciaFunction;
 
 public interface CinciaIterable extends Iterable<CinciaObject>, CinciaObject{
   	
-	public CinciaIterable filter(Predicate<CinciaObject> f);
-	public CinciaIterable map(UnaryOperator<CinciaObject> f);
 	
 	
-	CinciaIterable filter(PureCinciaFunction f);
+	CinciaIterable map(UnaryOperator<CinciaObject> f);
 	CinciaIterable map(PureCinciaFunction f);
+	CinciaIterable filter(Predicate<CinciaObject> f);
+	CinciaIterable filter(PureCinciaFunction f);
 	CinciaIterable reduce(PureCinciaFunction f, CinciaObject initial);
 	long size();
 }
