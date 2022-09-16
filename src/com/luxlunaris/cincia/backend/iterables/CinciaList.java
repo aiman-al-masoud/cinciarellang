ed9f136d143617cbe6eddb5e77a797a166c54c86
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-09-16 12:13:24 +0200

    ...

--- src/com/luxlunaris/cincia/backend/iterables/CinciaList.java
+++ src/com/luxlunaris/cincia/backend/iterables/CinciaList.java
@@ -159,7 +159,8 @@ public class CinciaList extends AbstractCinciaObject implements CinciaIterable {
 
 	@Override
 	public CinciaObject get(String key) {
-
+		
+		// size attribute 
 		if(key.equals(IterMethods.size.toString()) ) {
 			return new CinciaInt((int)size());
 		}
