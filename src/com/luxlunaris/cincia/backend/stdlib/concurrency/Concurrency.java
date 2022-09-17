0b1579738cad0d02b924abaa94cb68b4f8d95b59
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-09-17 20:05:39 +0200

    ...

--- src/com/luxlunaris/cincia/backend/stdlib/concurrency/Concurrency.java
+++ src/com/luxlunaris/cincia/backend/stdlib/concurrency/Concurrency.java
@@ -1,5 +1,9 @@
 package com.luxlunaris.cincia.backend.stdlib.concurrency;
 
+import java.util.List;
+
+import com.luxlunaris.cincia.backend.interfaces.CinciaObject;
+import com.luxlunaris.cincia.backend.primitives.CinciaString;
 import com.luxlunaris.cincia.backend.stdlib.Module;
 
 public class Concurrency extends Module{
@@ -8,6 +12,11 @@ public class Concurrency extends Module{
 		set("Promise", new Promise());
 		set("setTimeout", new SetTimeout());
 		set("sleep", new Sleep());
+	}
+	
+	@Override
+	public CinciaString help(List<CinciaObject> args) {
+		return new CinciaString("The concurrency module contains some tools that aid in developing simple multi-threaded applications.") ;
 	}
 
 }
