7199308afcf957fd66a7a71f3703680f9b8e853c
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-09-17 06:34:09 +0200

    ...

--- src/com/luxlunaris/cincia/frontend/ast/statements/labelled/CaseStatement.java
+++ src/com/luxlunaris/cincia/frontend/ast/statements/labelled/CaseStatement.java
@@ -5,21 +5,22 @@ import com.luxlunaris.cincia.frontend.ast.interfaces.Statement;
 import com.luxlunaris.cincia.frontend.ast.statements.CompoundStatement;
 
 public class CaseStatement implements Statement{
-	
-	
+
 	public Expression cond;
 	public CompoundStatement block;
-	
+	public Expression expression;
+
 	@Override
 	public Statement simplify() {
 		this.cond = cond.simplify();
-		this.block = (CompoundStatement) block.simplify();
+		this.block = block !=null?(CompoundStatement) block.simplify():block;
+		this.expression = expression!=null? expression.simplify() :expression;
 		return this;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "case "+cond+" "+block;
 	}
-	
+
 }
