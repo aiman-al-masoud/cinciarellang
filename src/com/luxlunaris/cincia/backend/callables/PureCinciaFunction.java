ae12cf662b9c4057b328b2d20bb5e31954f838af
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-09-28 20:07:36 +0200

    ...

--- src/com/luxlunaris/cincia/backend/callables/PureCinciaFunction.java
+++ src/com/luxlunaris/cincia/backend/callables/PureCinciaFunction.java
@@ -27,7 +27,7 @@ public class PureCinciaFunction extends CinciaFunction {
 
 	public CinciaObject run(List<CinciaObject> args) {
 		Enviro enviro = new Enviro(null); // brand new empty env
-		enviro.set(Magic.THIS.toString(), this);//reference to self, required to write recursive pure functions
+		enviro.set(Magic.THIS.toString(), this); //reference to self, required to write recursive pure functions
 		return super.run(args, enviro);
 	}
 
