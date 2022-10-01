62e9bf4c3e158addae8681ccf14f9580f691f545
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-10-01 18:28:33 +0200

    ...

--- src/com/luxlunaris/cincia/frontend/ast/expressions/type/PrimitiveType.java
+++ src/com/luxlunaris/cincia/frontend/ast/expressions/type/PrimitiveType.java
@@ -7,21 +7,20 @@ import com.luxlunaris.cincia.frontend.ast.interfaces.Type;
 import com.luxlunaris.cincia.frontend.ast.tokens.keyword.Keywords;
 
 public class PrimitiveType extends OneNameType{
-	
+
 	public static final Keywords INT = Keywords.INT;
 	public static final Keywords FLOAT = Keywords.FLOAT;
 	public static  final Keywords BOOL = Keywords.BOOL;
 	public static  final Keywords STRING = Keywords.STRING;
-	
 	public static  final Keywords MODULE = Keywords.MODULE;
 	public static  final Keywords ANY = Keywords.ANY;
-	
+
 	public Keywords value;
-	
+
 	public PrimitiveType(Keywords type) {
 		value = type;
 	}
-	
+
 	@Override
 	public String toString() {
 		return value+"";
@@ -29,35 +28,32 @@ public class PrimitiveType extends OneNameType{
 
 	@Override
 	public boolean matches(Type other) {
-		
-		
+
 		if(this.equals(Type.Any)) {
 			return true;
 		}
-		
-		
-		
+
 		other =other.unwrap();
-		
+
 		try {
 			return value.equals(((PrimitiveType)other).value);
 		}catch (ClassCastException e) {
-			
+
 		}
-		
+
 		return false;
-		
+
 	}
-	
+
 	@Override
 	public Type unwrap() {
 		return this;
 	}
-	
+
 	@Override
 	public Type resolve(Eval eval, Enviro enviro) {
 		return this;
 	}
-	
-	
+
+
 }
--- src/com/luxlunaris/cincia/frontend/ast/interfaces/Type.java
+++ src/com/luxlunaris/cincia/frontend/ast/interfaces/Type.java
@@ -6,23 +6,20 @@ import com.luxlunaris.cincia.frontend.ast.expressions.type.IdentifierType;
 import com.luxlunaris.cincia.frontend.ast.expressions.type.PrimitiveType;
 
 public interface Type extends Expression{
-	
-//	public static Type Any = new IdentifierType("Any");
-//	public static Type Module = new IdentifierType("Module");
 
 	public static Type Any  = new PrimitiveType(PrimitiveType.ANY);
 	public static Type Module  = new PrimitiveType(PrimitiveType.MODULE);
-	
+
 	boolean matches(Type other);
-	
-	
+
+
 	/**
 	 * TypeWrapper returns the wrapped type, everything elese
 	 * just returns this.
 	 * @return
 	 */
 	Type unwrap();
-	
+
 	/**
 	 * Resolve eventual nested user-defined Types within this Type.
 	 * 
@@ -31,6 +28,6 @@ public interface Type extends Expression{
 	 * @return
 	 */
 	Type resolve(Eval eval, Enviro enviro); // TODO: Stateful interface instead of Enviro class
-	
-	
+
+
 }
