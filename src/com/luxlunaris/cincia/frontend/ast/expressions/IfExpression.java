2ba92564ffb53812d878baef51a4e81daf36a045
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-10-07 18:06:55 +0200

    ...

--- src/com/luxlunaris/cincia/frontend/ast/expressions/IfExpression.java
+++ src/com/luxlunaris/cincia/frontend/ast/expressions/IfExpression.java
@@ -6,60 +6,38 @@ import com.luxlunaris.cincia.frontend.ast.interfaces.Statement;
 import com.luxlunaris.cincia.frontend.ast.statements.CompoundStatement;
 
 public class IfExpression implements Expression{
-	
+
 	public Expression cond;
-//	public CompoundStatement thenBlock;
-//	public CompoundStatement elseBlock;
-//	
-//	public Expression thenExpression;
-//	public Expression elseExpression;
-	
 	public Ast thenBranch;
 	public Ast elseBranch;
-	
+
 	@Override
 	public Expression simplify() {
 		this.cond  = cond.simplify();
-		
+
 		if(thenBranch!=null) {
 			this.thenBranch = thenBranch.simplify();
 		}
-		
+
 		if(elseBranch!=null) {
 			this.elseBranch = elseBranch.simplify();
 		}
-		
-//		if(thenBlock!=null) {
-//			this.thenBlock = (CompoundStatement) thenBlock.simplify();
-//		}
-//		
-//		if(elseBlock!=null) {
-//			this.elseBlock  = (CompoundStatement) elseBlock.simplify();
-//		}
-//		
-//		if(thenExpression!=null) {
-//			this.thenExpression = thenExpression.simplify();
-//		}
-//		
-//		if(elseExpression!=null) {
-//			this.elseExpression = elseExpression.simplify();
-//		}
-//		
+
 		return this;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "if "+cond+" then "+thenBranch+" else "+elseBranch;
 	}
-	
+
 	public Ast getThen() { //TODO: throw if both null?
 		return thenBranch;
 	}
-	
+
 	public Ast getElse() { //TODO: throw if both null?
 		return elseBranch;
 	}
-	
-	
+
+
 }
