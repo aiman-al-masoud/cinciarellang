ead2458d1f0961a8042abd0c4226f8c75a16a4d7
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-10-09 23:01:12 +0200

    ...

--- src/com/luxlunaris/cincia/backend/throwables/TypeError.java
+++ src/com/luxlunaris/cincia/backend/throwables/TypeError.java
@@ -3,29 +3,28 @@ package com.luxlunaris.cincia.backend.throwables;
 import com.luxlunaris.cincia.frontend.ast.interfaces.Type;
 
 public class TypeError extends CinciaException {
-	
+
 	public String lvalue;
 	public Type expected;
 	public Type got;
 	public String message;
-	
+
 	public TypeError(String msg) {
 		super(msg);
 		this.message = msg;
 	}
-	
+
 	public TypeError() {
-		
+		/* on purpose */
 	}
-	
-	
+
 	@Override
 	public String toString() {
-		
+
 		if(message!=null) {
 			return message;
 		}
-		
+
 		return "left value: '"+lvalue+"', of expected type: '"+expected+"', got type: '"+got+"'";
 	}
 
