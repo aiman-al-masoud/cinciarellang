5e0ed196363460541a2120fe42bba9b29f1ac5ee
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-10-15 16:23:16 +0200

    ...

--- src/com/luxlunaris/cincia/backend/primitives/CinciaFloat.java
+++ src/com/luxlunaris/cincia/backend/primitives/CinciaFloat.java
@@ -1,9 +1,7 @@
 package com.luxlunaris.cincia.backend.primitives;
 
-import com.luxlunaris.cincia.backend.interfaces.CinciaObject;
 import com.luxlunaris.cincia.backend.types.TypeWrapper;
 import com.luxlunaris.cincia.frontend.ast.expressions.type.PrimitiveType;
-import com.luxlunaris.cincia.frontend.ast.tokens.operator.Operators;
 
 //TODO: better error messages
 public class CinciaFloat extends CinciaNumber {
@@ -14,22 +12,7 @@ public class CinciaFloat extends CinciaNumber {
 		super(new TypeWrapper(new PrimitiveType(PrimitiveType.FLOAT)));
 		this.value = value;		
 	}
-//
-//	@Override
-//	public CinciaObject __add__(CinciaObject other) {
-//		return CinciaObject.wrap( Alu.perform(this.toJava(), other.toJava(), Operators.PLUS) );
-//	}
-//
-//	@Override
-//	public CinciaObject __mul__(CinciaObject other) {
-//		return CinciaObject.wrap( Alu.perform(this.toJava(), other.toJava(), Operators.ASTERISK) );
-//	}
-//
-//	@Override
-//	public CinciaObject __mod__(CinciaObject other) {
-//		return CinciaObject.wrap( Alu.perform(this.toJava(), other.toJava(), Operators.MOD) );
-//	}
-//	
+	
 	@Override
 	public Double toJava() {
 		return value;
