b6fcb657f308b098f4cba2d46368d55c9c1e678e
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-10-15 16:22:39 +0200

    ...

--- src/com/luxlunaris/cincia/backend/primitives/CinciaFloat.java
+++ src/com/luxlunaris/cincia/backend/primitives/CinciaFloat.java
@@ -14,22 +14,22 @@ public class CinciaFloat extends CinciaNumber {
 		super(new TypeWrapper(new PrimitiveType(PrimitiveType.FLOAT)));
 		this.value = value;		
 	}
-
-	@Override
-	public CinciaObject __add__(CinciaObject other) {
-		return CinciaObject.wrap( Alu.perform(this.toJava(), other.toJava(), Operators.PLUS) );
-	}
-
-	@Override
-	public CinciaObject __mul__(CinciaObject other) {
-		return CinciaObject.wrap( Alu.perform(this.toJava(), other.toJava(), Operators.ASTERISK) );
-	}
-
-	@Override
-	public CinciaObject __mod__(CinciaObject other) {
-		return CinciaObject.wrap( Alu.perform(this.toJava(), other.toJava(), Operators.MOD) );
-	}
-	
+//
+//	@Override
+//	public CinciaObject __add__(CinciaObject other) {
+//		return CinciaObject.wrap( Alu.perform(this.toJava(), other.toJava(), Operators.PLUS) );
+//	}
+//
+//	@Override
+//	public CinciaObject __mul__(CinciaObject other) {
+//		return CinciaObject.wrap( Alu.perform(this.toJava(), other.toJava(), Operators.ASTERISK) );
+//	}
+//
+//	@Override
+//	public CinciaObject __mod__(CinciaObject other) {
+//		return CinciaObject.wrap( Alu.perform(this.toJava(), other.toJava(), Operators.MOD) );
+//	}
+//	
 	@Override
 	public Double toJava() {
 		return value;
