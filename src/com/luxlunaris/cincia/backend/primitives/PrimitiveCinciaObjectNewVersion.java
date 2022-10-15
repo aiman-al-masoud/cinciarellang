feb61501ab97b317c2345ea04ee83a58b64fbc34
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-10-15 16:20:33 +0200

    ...

--- src/com/luxlunaris/cincia/backend/primitives/PrimitiveCinciaObjectNewVersion.java
+++ src/com/luxlunaris/cincia/backend/primitives/PrimitiveCinciaObjectNewVersion.java
@@ -5,6 +5,7 @@ import java.util.List;
 import com.luxlunaris.cincia.backend.interfaces.CinciaObject;
 import com.luxlunaris.cincia.backend.object.CinciaCinciaClass;
 import com.luxlunaris.cincia.frontend.ast.interfaces.Type;
+import com.luxlunaris.cincia.frontend.ast.tokens.operator.Operators;
 
 public class PrimitiveCinciaObjectNewVersion extends CinciaCinciaClass {
 	
@@ -20,5 +21,61 @@ public class PrimitiveCinciaObjectNewVersion extends CinciaCinciaClass {
 		return this;
 	}
 	
+	
+	
+	
+	
+	
+	
+	
+	
+
+	
+	
+	@Override
+	public CinciaObject __lt__(CinciaObject other) {
+		return CinciaObject.wrap(Alu.perform(this.toJava(), other.toJava(), Operators.LT));
+	}
+
+	@Override
+	public CinciaObject __gt__(CinciaObject other) {
+		return CinciaObject.wrap(Alu.perform(this.toJava(), other.toJava(), Operators.GT));
+	}
+
+	@Override
+	public CinciaObject __lte__(CinciaObject other) {
+		return CinciaObject.wrap(Alu.perform(this.toJava(), other.toJava(), Operators.LTE));
+	}
+
+	@Override
+	public CinciaObject __gte__(CinciaObject other) {
+		return CinciaObject.wrap(Alu.perform(this.toJava(), other.toJava(), Operators.GTE));
+	}
+
+	@Override
+	public CinciaObject __div__(CinciaObject other) {
+		return CinciaObject.wrap(Alu.perform(this.toJava(), other.toJava(), Operators.DIV));
+	}
+
+	@Override
+	public CinciaObject __sub__(CinciaObject other) {
+		return CinciaObject.wrap(Alu.perform(this.toJava(), other.toJava(), Operators.MINUS));
+	}
+
+	@Override
+	public CinciaBool __eq__(CinciaObject other) {
+		return (CinciaBool)CinciaObject.wrap(toJava().equals(other.toJava()));
+	}
+
+	@Override
+	public CinciaString __str__() {
+		return (CinciaString) CinciaObject.wrap(toJava()+"");
+	}
+
+	@Override
+	public CinciaBool __bool__() {
+		return (CinciaBool) CinciaObject.wrap(Alu.perform( this.toJava() , 0, Operators.NE));
+	}
+
 
 }
