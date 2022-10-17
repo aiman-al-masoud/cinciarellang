2ff78406bf668f4caaec9688320061fc589c480d
aiman-al-masoud <luxlunarislabs@gmail.com>
2022-10-17 09:36:54 +0200

    ...

--- src/com/luxlunaris/cincia/tests/parser/classes/SelectionTester.java
+++
@@ -1,9 +0,0 @@
-package com.luxlunaris.cincia.tests.parser.classes;
-
-public class SelectionTester extends AbstractTester {
-
-	public SelectionTester() {
-		add("if x { x = 1; y = 2;}else{y = x = 0; }", "");
-		add("match x{case 1:return 1;case 2:return 2;default:return 0;}", "");
-	}
-}
